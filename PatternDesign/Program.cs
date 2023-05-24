b5bbb20902c22dd8daf07f239e1f13433237f255
Sistemas Aczeda <alejimenezgithub@gmail.com>
2023-05-24 13:40:51 +0200

    Agregar archivos de proyecto.

---
+++ PatternDesign/Program.cs
@@ -0,0 +1,17 @@
+﻿using PatternDesign;
+
+Utils utils = new Utils();
+
+Utils.WriteConsole writeConsole = utils.writeConsole;
+Utils.AnotherThings anotherThings = utils.anotherThings; // Example
+
+writeConsole.WriteIntro();
+writeConsole.WriteInfo("This is my project for study pattern design.");
+
+
+writeConsole.WriteInfo("Now I'm executing the Factory Method.");
+new FactoryMethod().Main();
+
+
+writeConsole.WriteInfo("End of execute the project PatternDesign.", true);
+
---
+++ PatternDesign/Utils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternDesign
+{
+    public class Utils
+    {
+        public WriteConsole writeConsole;
+        public AnotherThings anotherThings;
+
+        // That is a constructor
+        public Utils()
+        {
+            writeConsole = new WriteConsole();
+            anotherThings = new AnotherThings();
+        }
+
+        public class WriteConsole
+        {
+            public WriteConsole() { }
+
+            public void WriteInfo(string textInfo, bool endProject = false)
+            {
+                if (endProject) JumpLine();
+                WriteLines();
+                Console.WriteLine(textInfo);
+                WriteLines();
+                JumpLine();
+            }
+
+            public void WriteIntro()
+            {
+                Console.Clear();
+                Console.Title = "Patter Design";
+            }
+
+            private void JumpLine()
+            {
+                Console.WriteLine("\r\n");
+            }
+
+            private void WriteLines()
+            {
+                Console.WriteLine("--------------------------------------------");
+            }
+        }
+
+        public class AnotherThings
+        {
+            public AnotherThings() { }
+        }
+    }
+}
