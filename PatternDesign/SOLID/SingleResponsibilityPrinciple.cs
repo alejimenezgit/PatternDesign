9bf81556cdbf83c472c237a16886c23e5e922593
alejimenezgit <alejimenezgithub@gmail.com>
2024-02-23 09:01:51 +0100

    New fields SOLID

---
+++ PatternDesign/SOLID/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple {
+    
+    internal class SingleResponsibilityPrinciple
+    { 
+        // Una clase debe hacer una cosa y, por lo tanto, debe tener una sola razón para cambiar
+
+        
+    }
+}
